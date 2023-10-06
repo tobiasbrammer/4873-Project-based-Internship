340815a2f5c162d8abc7799176dbad7b670db2b2
tobiasbrammer <70036361+tobiasbrammer@users.noreply.github.com>
2023-10-06 22:17:39 +0200

    .

--- Data/Scripts/2_EDA.py
+++ Data/Scripts/2_EDA.py
@@ -18,16 +18,30 @@ dfData = pd.read_parquet(f"{sDir}/dfData.parquet")
 
 # Convert the 'date' column to date format (assuming it's in 'YYYY-MM-DD' format)
 dfData['date'] = pd.to_datetime(dfData['date'], format='%Y-%m-%d')
+dfData['end_date'] = pd.to_datetime(dfData['end_date'], format='%Y-%m-%d')
 
 # Summary of Data
+# Format date and end_date as dd-mm-yyyy
+dfData['date'] = dfData['date'].dt.strftime('%d-%m-%Y')
+dfData['end_date'] = dfData['end_date'].dt.strftime('%d-%m-%Y')
 summary_data = dfData.describe().transpose()
 # Format all numerical values in DataFrame with thousands separator.
 formatted_df_eda_1 = summary_data.map(lambda x: '{:,.2f}'.format(x) if isinstance(x, (int, float)) else x)
 # Keep index, min, max, mean, std.
 formatted_df_eda_1 = formatted_df_eda_1[['mean', 'std', 'min', 'max']]
-formatted_df_eda_1.loc['date', 'mean'] = dfData['date'].min().strftime('%d-%m-%Y')
+# Count number of missing values for each variable
+missing_values = dfData.isnull().sum().to_frame()
+# Rename column
+missing_values = missing_values.rename(columns={0: 'Missing'})
+# Percentage of missing values
+missing_values['% Missing'] = missing_values['Missing'] / len(dfData) * 100
+# Add missing values to formatted_df_eda_1
+formatted_df_eda_1 = formatted_df_eda_1.join(missing_values)
+
 # Replace all percent signs % with \%
 eda_1 = formatted_df_eda_1.to_latex(index=True, caption='Variables',longtable=True,label='eda_1').replace('%', '\\%')
+# set font size
+eda_1 = eda_1.replace('small', 'footnotesize')
 eda_1 = eda_1.replace('NaN', 'Missing')
 # Save as LaTeX using Styler
 with open('./Results/Tables/2_eda_1.tex', 'w', encoding='utf-8') as f:
