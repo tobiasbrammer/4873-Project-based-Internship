901681d230a6edc03f66fa817e08c063c6c06f47
tobiasbrammer <70036361+tobiasbrammer@users.noreply.github.com>
2023-11-24 14:13:05 +0100

    .

--- Data/smape.py
+++ Data/smape.py
@@ -1,3 +1,3 @@
 def smape(actual, predicted):
     import numpy as np
-    return 100 / len(actual) * np.sum(np.abs(predicted - actual) / (np.abs(actual) + np.abs(predicted)))
+    return 100 / len(actual) * np.sum(np.abs(actual - predicted) / (np.abs(actual) + np.abs(predicted)))
