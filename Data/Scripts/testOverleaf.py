fd80db45944e7e64ae5185858d04393c7f3f0664
tobiasbrammer <70036361+tobiasbrammer@users.noreply.github.com>
2023-12-06 14:17:42 +0100

    .

--- Data/Scripts/testOverleaf.py
+++
@@ -1,6 +0,0 @@
-import pyoverleaf
-api = pyoverleaf.Api()
-api.login_from_browser()
-projects = api.get_projects()
-project_id = projects[0].id
-rootdir = api.project_get_files(project_id)
--- Data/smape 2.py
+++
@@ -1,3 +0,0 @@
-def smape(actual, predicted):
-    import numpy as np
-    return 100 / len(actual) * np.sum(np.abs(actual - predicted) / (np.abs(actual) + np.abs(predicted)))
